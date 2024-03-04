ae96a433af042bb3c38729ecbeb49cd760307e06
JoshuaEbbert <41241922+JoshuaEbbert@users.noreply.github.com>
2024-03-04 15:36:43 -0700

    file restructuring and adding index.js

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-function login() {
-    const nameEl = document.querySelector('#username');
-    localStorage.setItem('username', nameEl.value);
-    window.location.href = 'chat.html';
-}
+const express = require('express');
+const app = express();
+
+// The service port. In production the frontend code is statically hosted by the service on the same port.
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+// JSON body parsing using built-in middleware
+app.use(express.json());
+
+// Serve up the frontend static content hosting
+app.use(express.static('public'));
+
+// Return the application's default page if the path is unknown
+app.use((_req, res) => {
+    res.sendFile('index.html', { root: 'public' });
+  });
+  
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
