3127b4fe9d46cc762e2d315e28a113ce2a7c7ec1
JoshuaEbbert <41241922+JoshuaEbbert@users.noreply.github.com>
2024-03-19 21:01:55 -0600

    expanding login() in public/index.js to include account creation logic. wip

--- public/index.js
+++ public/index.js
@@ -1,5 +1,23 @@
-function login() {
-    const nameEl = document.querySelector('#username');
-    localStorage.setItem('username', nameEl.value);
-    window.location.href = 'chat.html';
+async function authenticateCredentials(url) {
+    let username = document.querySelector('#username').value;
+    let password = document.querySelector('#password').value;
+
+    const response = await fetch(url, {
+        method: 'post',
+        body: JSON.stringify({ username: username, password: password }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+    });
+
+    if (response.ok) {
+        localStorage.setItem('username', username);
+        window.location.href = 'chat.html';
+    } else {
+        const body = await response.json();
+        const modalEl = document.querySelector('#msgModal');
+        modalEl.querySelector('.modal-body').textContent = `⚠ Error: ${body.msg}`;
+        const msgModal = new bootstrap.Modal(modalEl, {});
+        msgModal.show();
+    }
 }
