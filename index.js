4f0a7facd67cfe4e660cd495d3dc375edfa26e37
JoshuaEbbert <41241922+JoshuaEbbert@users.noreply.github.com>
2024-03-21 12:29:21 -0600

    boilerplate ws code

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcrypt');
 const express = require('express');
 const app = express();
 const DB = require('./database.js');
+const { peerProxy } = require('./peerProxy.js');
 
 const authCookieName = 'token';
 
@@ -104,3 +105,9 @@ function setAuthCookie(res, authToken) {
     sameSite: 'strict',
   });
 }
+
+// const httpService = app.listen(port, () => {
+//   console.log(`Listening on port ${port}`);
+// });
+
+// peerProxy(httpService);
