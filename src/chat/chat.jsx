5ccb66993ae6e13fd4d3d0400c9308dd695f745b
JoshuaEbbert <41241922+JoshuaEbbert@users.noreply.github.com>
2024-04-04 08:27:12 -0600

    added chat, about, trending, and login.jsx stubs in corresponding folders

---
+++ src/chat/chat.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Chat() {
+  return (
+    <main className='container-fluid bg-secondary text-center'> {/* change/remove className tags? */}
+      <div>chat displayed here</div>
+    </main>
+  );
+}
---
+++ src/trending/trending.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Trending() {
+  return (
+    <main className='container-fluid bg-secondary text-center'> {/* change/remove className tags? */}
+      <div>Trending displayed here</div>
+    </main>
+  );
+}
