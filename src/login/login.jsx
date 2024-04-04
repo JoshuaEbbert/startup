e3650512755f4cc42a85112c2a26071e35538947
JoshuaEbbert <41241922+JoshuaEbbert@users.noreply.github.com>
2024-04-04 10:46:36 -0600

    login wip. added authenticated and unauthenticated.jsx (latter empty)

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import './login.css';
 
-export function Login() {
+import { Unauthenticated } from './unauthenticated';
+import { Authenticated } from './authenticated';
+import { AuthState } from './authState';
+
+export function Login({ userName, authState, onAuthChange }) {
   return (
     <main className="login"> 
-      <div>login displayed here</div>
+        <div>
+            {authState === AuthState.Authenticated && (
+                <Authenticated 
+                    userName={userName} 
+                    onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)} 
+                />
+            )}
+            {authState === AuthState.Unauthenticated && (
+                <Unauthenticated
+                    userName={userName}
+                    onLogin={(loginUserName) => {
+                        onAuthChange(loginUserName, AuthState.Authenticated);
+                    }}
+                />
+            )}
+        </div>
     </main>
   );
 }
---
+++ src/login/unauthenticated.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import Button from 'react-bootstrap/Button';
+import {MessageDialog} from './messageDialog';
+
+export function Unauthenticated(props) {
+}
