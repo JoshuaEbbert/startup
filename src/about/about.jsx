bf03685b316b2a1b7517b1a0e660aa3f58c8ed31
JoshuaEbbert <41241922+JoshuaEbbert@users.noreply.github.com>
2024-04-04 10:10:55 -0600

    username checks and authState.js added

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './about.css';
 
 export function About() {
   return (
     <main className="about">
-      <div>about displayed here</div>
+        <div class="p-5 text-center bg-body-tertiary rounded-3">
+            <img alt="Test prep books" src="background.jpeg" class="center-image"/>
+            <h1 class="text-body-emphasis m-5">Embrace Efficiency Over Exertion</h1>
+            <p class="col-lg-8 mx-auto fs-5 text-muted">
+                TestPrep StrateGPT is a chat application that uses the GPT-3 model to help students prepare for college entrance exams. 
+                It is designed to help students study for exams by providing them with a chatbot that can answer questions and provide explanations for difficult concepts.
+                The chatbot is trained on a wide range of topics and can provide detailed explanations for a variety of subjects, including math, science, history, and more. 
+                TestPrep StrateGPT is a powerful tool for students who want to improve their test scores and succeed in their academic pursuits.
+            </p>
+        </div>
     </main>
   );
 }
--- src/app.jsx
+++ src/app.jsx
@@ -4,64 +4,69 @@ import { Login } from './login/login';
 import { Chat } from './chat/chat';
 import { Trending } from './trending/trending';
 import { About } from './about/about';
+import { AuthState } from './login/authState';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
-export default function App() {
-  return (
-    <BrowserRouter>
-        <div className="body">
-            <header className="d-flex flex-wrap justify-content-center py-3 border-bottom">
-                <div href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1 pl-25 align-items-center">
-                <img src="iconStrateGPT.svg" alt="Icon StrateGPT" width="45" height="45"></img>
-                </div>
-                <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-                <span className="fs-4">Test Prep StrateGPT<sup>&reg;</sup></span>
-                </a>
+function App() {
+    const [userName, setUserName] = React.useState(localStorage.getItem('userName') || 'No username provided');
+    const currentAuthState = userName === 'No username provided' ? AuthState.Authenticated : AuthState.Unauthenticated;
+    const [authState, setAuthState] = React.useState(currentAuthState);
 
-                <div className="header-list pr-25">
-                <ul className="nav nav-pills">
-                    <li className="nav-item"><NavLink to="login" className="nav-link" activeClassName="active" aria-current="page">Login</NavLink></li>
-                    <li className="nav-item"><NavLink to="chat" className="nav-link">Chat</NavLink></li>
-                    <li className="nav-item"><NavLink to="trending" className="nav-link">Trending</NavLink></li>
-                    <li className="nav-item"><NavLink to="about" className="nav-link">About</NavLink></li>
-                </ul>
-                </div>
-            </header>
-
-            <Routes>
-                <Route path='/' element={<Login />} exact />
-                <Route path='/login' element={<Login />} />
-                <Route path='/chat' element={<Chat />} />
-                <Route path='/trending' element={<Trending />} />
-                <Route path='/about' element={<About />} />
-                <Route path='*' element={<NotFound />} />
-            </Routes>
-
-            <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 border-top">
-                <div className="col-md-4 d-flex align-items-center pl-25">
-                    <a href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-                        <img src="iconStrateGPT.svg" alt="Icon StrateGPT" width="24" height="24"></img>
+    return (
+        <BrowserRouter>
+            <div className="body">
+                <header className="d-flex flex-wrap justify-content-center py-3 border-bottom">
+                    <div href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1 pl-25 align-items-center">
+                    <img src="iconStrateGPT.svg" alt="Icon StrateGPT" width="45" height="45"></img>
+                    </div>
+                    <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+                    <span className="fs-4">Test Prep StrateGPT<sup>&reg;</sup></span>
                     </a>
-                    <span className="mb-3 mb-md-0 text-muted">© 2024 TestPrep StrateGPT</span>
-                </div>
 
-                <div className="align-items-center justify-content-center">
-                    <span className="ms-3 text-muted mobile-disappear username-display"></span>
-                </div>
+                    <div className="header-list pr-25">
+                    <ul className="nav nav-pills">
+                        <li className="nav-item"><NavLink to="login" className="nav-link" activeClassName="active" aria-current="page">Login</NavLink></li>
+                        <li className="nav-item"><NavLink to="chat" className="nav-link">Chat</NavLink></li>
+                        <li className="nav-item"><NavLink to="trending" className="nav-link">Trending</NavLink></li>
+                        <li className="nav-item"><NavLink to="about" className="nav-link">About</NavLink></li>
+                    </ul>
+                    </div>
+                </header>
 
-                <ul className="nav col-md-4 justify-content-end list-unstyled d-flex pr-25">
-                    <li className="ms-3 text-muted mobile-disappear">Joshua Ebbert</li>
-                    <li className="ms-3 mobile-disappear"><a className="text-muted" href="https://github.com/JoshuaEbbert/startup/tree/main">Source Code</a></li>
-                </ul>
-            </footer>
+                <Routes>
+                    <Route path='/' element={<Login />} exact />
+                    <Route path='/login' element={<Login />} />
+                    <Route path='/chat' element={<Chat />} />
+                    <Route path='/trending' element={<Trending />} />
+                    <Route path='/about' element={<About />} />
+                    <Route path='*' element={<NotFound />} />
+                </Routes>
 
-            <script src="init.js"></script>
-        </div>
-    </BrowserRouter>
-);
+                <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 border-top">
+                    <div className="col-md-4 d-flex align-items-center pl-25">
+                        <a href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+                            <img src="iconStrateGPT.svg" alt="Icon StrateGPT" width="24" height="24"></img>
+                        </a>
+                        <span className="mb-3 mb-md-0 text-muted">© 2024 TestPrep StrateGPT</span>
+                    </div>
+
+                    <div className="align-items-center justify-content-center">
+                        <span className="ms-3 text-muted mobile-disappear username-display">{userName}</span>
+                    </div>
+
+                    <ul className="nav col-md-4 justify-content-end list-unstyled d-flex pr-25">
+                        <li className="ms-3 text-muted mobile-disappear">Joshua Ebbert</li>
+                        <li className="ms-3 mobile-disappear"><a className="text-muted" href="https://github.com/JoshuaEbbert/startup/tree/main">Source Code</a></li>
+                    </ul>
+                </footer>
+            </div>
+        </BrowserRouter>
+    ); 
 }
 
 function NotFound() {
-    return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
+    return <main>404: Return to sender. Address unknown.</main>;
 }
+
+export default App;
