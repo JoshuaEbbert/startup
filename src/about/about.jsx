641ce747566e6c34ff4b02297d70c06528465fed
JoshuaEbbert <41241922+JoshuaEbbert@users.noreply.github.com>
2024-04-08 12:24:25 -0600

    added message dialog. Fixed two instances of 'class' in About.jsx

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -7,8 +7,8 @@ export function About() {
     <main className="about">
         <div class="p-5 text-center bg-body-tertiary rounded-3">
             <img alt="Test prep books" src="background.jpeg" class="center-image"/>
-            <h1 class="text-body-emphasis m-5">Embrace Efficiency Over Exertion</h1>
-            <p class="col-lg-8 mx-auto fs-5 text-muted">
+            <h1 className="text-body-emphasis m-5">Embrace Efficiency Over Exertion</h1>
+            <p className="col-lg-8 mx-auto fs-5 text-muted">
                 TestPrep StrateGPT is a chat application that uses the GPT-3 model to help students prepare for college entrance exams. 
                 It is designed to help students study for exams by providing them with a chatbot that can answer questions and provide explanations for difficult concepts.
                 The chatbot is trained on a wide range of topics and can provide detailed explanations for a variety of subjects, including math, science, history, and more. 
--- src/login/unauthenticated.jsx
+++ src/login/unauthenticated.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button'; {/* Reformat to include Button object if styling turns out poorly */}
+import {MessageDialog} from './messageDialog';
 
 export function Unauthenticated(props) {
     const [username, setUsername] = React.useState(props.username);
     const [password, setPassword] = React.useState('');
+    const [displayError, setDisplayError] = React.useState(null);
 
     const login = () => {
         if (username !== '' && password !== '') {
@@ -34,24 +36,28 @@ export function Unauthenticated(props) {
             props.onLogin(username);
         } else {
             const body = await response.json();
-            {/* TODO: implement alert */}
+            setDisplayError(`⚠ Error: ${body.msg}`);
         }
     }
 
     return (
-        <div className="login-box">
-            <h1 className="mb-3">Welcome</h1>
-            <div className="login-form">
-                <label htmlFor="username">Username</label>
-                <input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Type username here" />
-                <label htmlFor="password">Password</label>
-                <input type="text" id="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Type password here" />
+        <>
+            <div className="login-box">
+                <h1 className="mb-3">Welcome</h1>
+                <div className="login-form">
+                    <label htmlFor="username">Username</label>
+                    <input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Type username here" />
+                    <label htmlFor="password">Password</label>
+                    <input type="text" id="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Type password here" />
 
-                <div className="m-3 d-flex justify-content-between">
-                    <button style={{margin: "2px"}} className="btn btn-primary gap-3" onClick={login}>Login</button>
-                    <button style={{margin: "2px"}} className="btn btn-primary gap-3" onClick={create}>Create Account</button>
+                    <div className="m-3 d-flex justify-content-between">
+                        <button style={{margin: "2px"}} className="btn btn-primary gap-3" onClick={login}>Login</button>
+                        <button style={{margin: "2px"}} className="btn btn-primary gap-3" onClick={create}>Create Account</button>
+                    </div>
                 </div>
             </div>
-        </div>
+
+            <MessageDialog message={displayError} onHide={() => setDisplayError(null)} />
+        </>
     );
 }
